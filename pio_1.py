9e3ae5192c7ff04e91ef8a09dbc7be5f4067f754
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2023-11-08 20:21:03 +0000

    added button press to stop state machine

--- pio_1.py
+++ pio_1.py
@@ -3,21 +3,33 @@
 import time
 import rp2
 from machine import Pin
-@rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
+@rp2.asm_pio(set_init=(rp2.PIO.OUT_LOW, rp2.PIO.OUT_HIGH, rp2.PIO.OUT_LOW))
 def blink():
     wrap_target()
-    set(pins, 1)   [31]
+    set(pins, 0b100)   [31]
     nop()          [31]
     nop()          [31]
     nop()          [31]
-    nop()          [31]
-    set(pins, 0)   [31]
-    nop()          [31]
-    nop()          [31]
+    nop()          [31]    
+    set(pins, 0b010)   [31]
     nop()          [31]
     nop()          [31]
+    nop()          [31]
+    nop()          [31]    
+    set(pins, 0b001)   [31]
+    nop()          [31]
+    nop()          [31]
+    nop()          [31]
+    nop()          [31]    
     wrap()
-sm = rp2.StateMachine(0, blink, freq=2000, set_base=Pin(27))
+sm = rp2.StateMachine(0, blink, freq=2000, set_base=Pin(26))
 sm.active(1)
-time.sleep(3)
-sm.active(0)
+button = Pin(22, Pin.IN)
+
+while 1 == 1:
+    if button.value() == 1:
+        sm.active(0)
+        time.sleep(3)
+        sm.active(1)
+        
+    
