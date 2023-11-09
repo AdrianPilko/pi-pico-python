0a90ae7daad82a3029acc7cdc98f54984dbfbfba
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2023-11-09 16:40:57 +0000

    added interrupt driven button action

--- pio_1.py
+++ pio_1.py
@@ -15,21 +15,34 @@ def blink():
     nop()          [31]
     nop()          [31]
     nop()          [31]
-    nop()          [31]    
+    nop()          [31]
     set(pins, 0b001)   [31]
     nop()          [31]
     nop()          [31]
     nop()          [31]
-    nop()          [31]    
+    nop()          [31]
+    set(pins, 0b010)[31]
+    nop()          [31]
+    nop()          [31]
+    nop()          [31]
+    nop()          [31]          
     wrap()
+    
 sm = rp2.StateMachine(0, blink, freq=2000, set_base=Pin(26))
 sm.active(1)
-button = Pin(22, Pin.IN)
+
+buttonOn = Pin(22, Pin.IN, pull=Pin.PULL_UP)
+buttonOff = Pin(21, Pin.IN, pull=Pin.PULL_UP)
+
+def handleButtonOn(pin):
+    sm.active(0)
+
+def handleButtonOff(pin):
+    sm.active(1)
+        
+buttonOn.irq(trigger=Pin.IRQ_RISING,handler=handleButtonOn)
+buttonOff.irq(trigger=Pin.IRQ_RISING,handler=handleButtonOff)
 
 while 1 == 1:
-    if button.value() == 1:
-        sm.active(0)
-        time.sleep(3)
-        sm.active(1)
-        
+    time.sleep(10)        
     
