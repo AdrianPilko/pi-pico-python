556bae524a27c45207b6c4c6d04e1cb7fbbca315
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2024-03-02 17:13:17 +0000

    testPinsPIO
    with some code initially from https://github.com/raspberrypi/pico-micropython-examples/blob/master/pio/pio_1hz.py just to get something working, will change to proper PIO for 1541

--- pico-c64-1541-drive.py
+++ pico-c64-1541-drive.py
@@ -1,4 +1,5 @@
 from machine import Pin
+import rp2 
 import time
 import select
 import sys
@@ -8,15 +9,16 @@ import utime
 IEC_TRUE = 0
 IEC_FALSE = 1
 
+IEC_PIN_CLOCK = 2
+IEC_PIN_DATA = 3
+IEC_PIN_RESET =4
+IEC_PIN_ATN = 5
+
+
 led = Pin(13, Pin.OUT)
 ledClock = Pin(10, Pin.OUT)
 ledData = Pin(11, Pin.OUT)
 ledATN = Pin(12, Pin.OUT)
-IEC_1541_CLOCK = Pin(2,Pin.IN)
-IEC_1541_DATA = Pin(3,Pin.IN)
-IEC_1541_RESET = Pin(4,Pin.IN)
-IEC_1541_ATN = Pin(5,Pin.IN)
-
 
 def testPins():
     currentClock = IEC_1541_CLOCK.value()
@@ -56,25 +58,58 @@ def testPins():
             ledATN.value(currentATN)
         
         loopCount = loopCount + 1
-        
+
+
+@rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
+def blink_1hz():
+    # Cycles: 1 + 1 + 6 + 32 * (30 + 1) = 1000
+    irq(rel(0))
+    set(pins, 1)
+    set(x, 31)                  [5]
+    label("delay_high")
+    nop()                       [29]
+    jmp(x_dec, "delay_high")
+
+    # Cycles: 1 + 7 + 32 * (30 + 1) = 1000
+    set(pins, 0)
+    set(x, 31)                  [6]
+    label("delay_low")
+    nop()                       [29]
+    jmp(x_dec, "delay_low")
+
+def testPinsPIO():
+    # Create the StateMachine with the blink_1hz program, outputting on Pin(25).
+    sm = rp2.StateMachine(0, blink_1hz, freq=2000, set_base=Pin(13))
+
+    # Set the IRQ handler to print the millisecond timestamp.
+    sm.irq(lambda p: print(time.ticks_ms()))
+
+    # Start the StateMachine.
+    sm.active(1)
 
 def waitClock_TRUE():
+    IEC_1541_CLOCK = Pin(IEC_PIN_CLOCK,Pin.IN)
     currentClock = IEC_1541_CLOCK.value()
     while  currentClock == IEC_FALSE:
         currentClock = IEC_1541_CLOCK.value()        
 def waitClock_FALSE():
+    IEC_1541_CLOCK = Pin(IEC_PIN_CLOCK,Pin.IN)
     currentClock = IEC_1541_CLOCK.value()
     while  currentClock == IEC_TRUE:
         currentClock = IEC_1541_CLOCK.value()        
 def setData_TRUE():
+    IEC_1541_DATA = Pin(IEC_PIN_CLOCK,Pin.OUT)
     IEC_1541_DATA.value(IEC_TRUE)
 def setData_FALSE():
+    IEC_1541_DATA = Pin(IEC_PIN_CLOCK,Pin.OUT)
     IEC_1541_DATA.value(IEC_FALSE)    
 def waitATN_IEC_TRUE():
+    IEC_1541_ATN = Pin(IEC_PIN_ATN,Pin.IN)
     currentATN = IEC_1541_ATN.value()
     while  currentATN == IEC_FALSE:
         currentATN = IEC_1541_ATN.value()        
 def waitATN_IEC_FALSE():
+    IEC_1541_ATN = Pin(IEC_PIN_ATN,Pin.IN)
     currentATN = IEC_1541_ATN.value()
     while  currentATN == IEC_TRUE:
         currentATN = IEC_1541_ATN.value()        
@@ -118,7 +153,11 @@ def readData():
     return data     
 
 ### MAIN routine   
-##testPins()
+#testPins()
+
+testPinsPIO()
+## if testPinsPIO() is uncommented then never gets here
+
 loopCount = 0
 
 while True:
