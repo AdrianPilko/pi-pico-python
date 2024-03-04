c906a22a3f21b85e9f81878e46b03beb3f307162
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2024-03-04 19:35:01 +0000

    Update pico-c64-1541-drive.py

--- pico-c64-1541-drive.py
+++ pico-c64-1541-drive.py
@@ -50,54 +50,14 @@ IEC_FALSE = 1
 
 IEC_PIN_CLOCK = 2
 IEC_PIN_DATA = 3
-IEC_PIN_RESET =4
-IEC_PIN_ATN = 5
+IEC_PIN_ATN = 4
+IEC_PIN_RESET = 5 
 
 ## some pins for LED to enable quick debugging
 ledClock = Pin(10, Pin.OUT)
 ledData = Pin(11, Pin.OUT)
 ledATN = Pin(12, Pin.OUT)
 ledHeartbeat = Pin(13, Pin.OUT)
-
-## quick function to check connections - but uses GPIO not PIO!
-def testPins():
-    currentClock = IEC_1541_CLOCK.value()
-    currentData = IEC_1541_DATA.value()
-    currentATN = IEC_1541_ATN.value()
-    currentReset = IEC_1541_RESET.value()
-
-    oldClock = currentClock
-    oldData = currentData
-    oldATN = currentATN
-    oldReset = currentReset
-
-    ioContinuing = 1
-    loopCount = 0
-
-    while ioContinuing != 0:
-        
-        # slow down heartbeat led toggle
-        if loopCount % 5000 == 0:
-            ledHeartbeat.toggle()
-            
-        oldClock = currentClock
-        oldData = currentData
-        oldATN = currentATN
-        oldReset = currentReset
-            
-        currentClock = IEC_1541_CLOCK.value()
-        currentData = IEC_1541_DATA.value()
-        currentATN = IEC_1541_ATN.value()
-        currentReset = IEC_1541_RESET.value()
-        
-        if (oldClock != currentClock):
-            ledClock.value(currentClock)
-        if (oldData != currentData):
-            ledData.value(currentData)
-        if (oldATN != currentATN):
-            ledATN.value(currentATN)
-        
-        loopCount = loopCount + 1
 
 #read clock PIO waits for clock rising from 0 (true) to 1 (false) then siganls IRQ
 @rp2.asm_pio()
@@ -113,43 +73,35 @@ def readClockPinPIO():
 @rp2.asm_pio()
 def handleCBM_BusLowLevel():
     ## Base pin 0 should be IEC_PIN_CLOCK == GPIO 2
-    set(pins, 0b00001001)  # Set pin 0 and 3 as inputs
+    set (pindirs, 0b00000010)  # Set clock 0 and atn 2 as inputs, pin 1 data is an output
     wrap_target()    
-    wait(3, pin, 0)   # wait for ATN pin to go  IEC_TRUE = 0 (note not final solution)
-    irq(block, rel(0))    
+    wait(0, pin, 2)   # wait for ATN pin to go  IEC_TRUE = 0 (note not final solution)    
     wait(0, pin, 0)   # wait for clock pin to go  IEC_TRUE = 0
-    wait(0, pin, 1)   # wait for clock pin to go  IEC_FALSE = 1 rising edge
-    out(pins, 1)     [31]   # output high to data pin
-    set(pins, 0b00001011)      # set data pin to input
-    set(x,7)          # setup read loop count 8 bits into x    
-    label("bitReadLoop")
-    wait(0, pin, 0)   # wait while the clock pin IEC_TRUE = 0    
-    wait(0, pin, 1)   # wait for clock pin to go  IEC_FALSE = 1 rising edge
+    wait(1, pin, 0)   # wait for clock pin to go  IEC_FALSE = 1 rising edge
+
+    set(x,1)
+    mov(osr,x)
+    out(pins, 1)   [31]           # set data pin to high and wait 31 clock cycles
+    set(pindirs, 0b00000000)      # set all pins to input
     
+    set(x,8)          # setup read loop count 8 bits into x
     
-    pull()   ###locks up if this is commented in
-    
-    
-    mov(y, osr)
-    mov(isr, y)
-    push()   
+    wait(0, pin, 0)   # wait for clock pin IEC_TRUE = 0    
+    label("bitReadLoop")  
+    wait(1, pin, 0)   # wait for clock pin to go  IEC_FALSE = 1 rising edge
+    in_(1, 1)         # read 1 bit from pin 1 (Data)   
+    wait(0, pin, 0)   # wait for the clock pin IEC_TRUE = 0    
     jmp(x_dec, "bitReadLoop")
-    irq(block, rel(0))    
-    set(pindirs, 2)  [31]    # set data pin to output
-    out(pins, 1)
-    set(pins, 2)  [31]    # set data pin to input
-    #irq(block, rel(0))    
+    
+    push(block)            # push the 8 bits to the cpu    
+    set(pindirs, 0b00000010)    # set data pin to output
+    set(x,1)
+    mov(osr,x)
+    out(pins, 1)   [31]      
     wrap()
 
-
-def handler(sm):
-    # Print a (wrapping) timestamp, and the state machine object.
-    print(time.ticks_ms(), sm)   
-
 basePin = Pin(IEC_PIN_CLOCK)
-#sm0 = rp2.StateMachine(0, handleCBM_BusLowLevel, in_base=basePin)
-sm0 = rp2.StateMachine(0, readClockPinPIO,freq=2000, in_base=basePin)
-
+sm0 = rp2.StateMachine(0, handleCBM_BusLowLevel,freq=125_000_000, in_base=basePin)
 # Function to read from FIFO
 def read_fifo():
     while True:
@@ -158,108 +110,11 @@ def read_fifo():
 def testPinsPIO():
 ### base pin 2 = CLOCK = 0
 ###      pin 3 = DATA  = 1
-###      pin 4 = RESET = 2
-###      pin 5 = ATN   = 3
-    # Instantiate StateMachine(0) with base pin 2 (CLOCK)
-    #basePin = Pin(IEC_PIN_CLOCK)
-    #sm0 = rp2.StateMachine(0, handleCBM_BusLowLevel, in_base=basePin)
-    sm0.irq(handler)
+###      pin 4 = ATN   = 2
+###      pin 5 = RESET = 3     ### not used
     sm0.active(1)    
-    #while True:
-    #    for data in read_fifo():
-    #        print("Received:", data)
-            
-    
-    
-
-def waitClock_TRUE():
-    IEC_1541_CLOCK = Pin(IEC_PIN_CLOCK,Pin.IN)
-    currentClock = IEC_1541_CLOCK.value()
-    while  currentClock == IEC_FALSE:
-        currentClock = IEC_1541_CLOCK.value()        
-def waitClock_FALSE():
-    IEC_1541_CLOCK = Pin(IEC_PIN_CLOCK,Pin.IN)
-    currentClock = IEC_1541_CLOCK.value()
-    while  currentClock == IEC_TRUE:
-        currentClock = IEC_1541_CLOCK.value()        
-def setData_TRUE():
-    IEC_1541_DATA = Pin(IEC_PIN_CLOCK,Pin.OUT)
-    IEC_1541_DATA.value(IEC_TRUE)
-def setData_FALSE():
-    IEC_1541_DATA = Pin(IEC_PIN_CLOCK,Pin.OUT)
-    IEC_1541_DATA.value(IEC_FALSE)    
-def waitATN_IEC_TRUE():
-    IEC_1541_ATN = Pin(IEC_PIN_ATN,Pin.IN)
-    currentATN = IEC_1541_ATN.value()
-    while  currentATN == IEC_FALSE:
-        currentATN = IEC_1541_ATN.value()        
-def waitATN_IEC_FALSE():
-    IEC_1541_ATN = Pin(IEC_PIN_ATN,Pin.IN)
-    currentATN = IEC_1541_ATN.value()
-    while  currentATN == IEC_TRUE:
-        currentATN = IEC_1541_ATN.value()        
-
-def readData():
-    IEC_1541_CLOCK = Pin(2,Pin.IN)
-    IEC_1541_DATA = Pin(3,Pin.OUT)
-    
-    waitClock_TRUE()
-    
-    setData_TRUE()
-    timeBefore =  utime.ticks_us()
-    waitClock_FALSE()
-    timeAfter =  utime.ticks_us()
-    
-    if (timeAfter - timeBefore > 200):      
-        IEC_1541_DATA = Pin(3,Pin.OUT)
-        setData_TRUE()
-        timeBefore =  utime.ticks_us()
-        timeAfter = 0
-        while timeAfter - timeBefore < 60:
-            timeAfter =  utime.ticks_us()
-        #sys.stdout.write('EOI ')        
-        #sys.stdout.write(str(timeAfter - timeBefore))
-        #sys.stdout.write('\n')            
-        return 0x00
-    
-    IEC_1541_DATA = Pin(3,Pin.IN)
-    
-    data = 0x00;
-    
-    for i in range(1, 8):
-        waitClock_FALSE()  
-        pinVal = IEC_1541_DATA.value() & 0x01
-        data = data | pinVal
-        data = data << 1; 
-        waitClock_TRUE()
-    
-    IEC_1541_DATA = Pin(3,Pin.OUT)
-    setData_TRUE() ## acknowledge the 8 bits
-    return data     
-
-### MAIN routine   
-#testPins()
+    while True:
+        for data in read_fifo():
+            print("Received:", data & 0xff)
 
 testPinsPIO()
-## if testPinsPIO() is uncommented then never gets here
-
-loopCount = 0
-
-while True:
-
-    # slow down heartbeat led toggle
-    #if loopCount % 5000 == 0:
-    waitATN_IEC_TRUE()
-           
-    
-    if (readData() & 0xff == 0x28):
-        # Send the data
-        sys.stdout.write('0x28\n')    
-   
-    ledHeartbeat.toggle()
-    waitATN_IEC_FALSE()
-    #loopCount = loopCount + 1
-                
-    
-    
- 
