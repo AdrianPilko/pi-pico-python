3583e0c479627b6dad17b443810f9355989a1856
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2024-03-08 13:27:17 +0000

    added another pio debug program

---
+++ pinDebug_pythonSDKfault_question.py
@@ -0,0 +1,33 @@
+from machine import Pin
+from rp2 import PIO, StateMachine, asm_pio
+import time
+
+#rp2.asm_pio(*, out_init=None, set_init=None, sideset_init=None, in_shiftdir=0, out_shiftdir=0, autopush=False, autopull=False, push_thresh=32, pull_thresh=32, fifo_join=PIO.JOIN_NONE)¶
+#Assemble a PIO program.
+#The following parameters control the initial state of the GPIO pins, as one of
+# PIO.IN_LOW, PIO.IN_HIGH, PIO.OUT_LOW or PIO.OUT_HIGH. If the program uses more
+# than one pin, provide a tuple, e.g. out_init=(PIO.OUT_LOW, PIO.OUT_LOW).
+#out_init configures the pins used for out() instructions.
+#set_init configures the pins used for set() instructions. There can be at most 5.
+
+@asm_pio(set_init=(PIO.OUT_LOW)) #, PIO.OUT_LOW))
+#set(dest, data)
+#set dest with the value data.
+#   dest: pins, x, y, pindirs
+#   data: value (0-31)
+def myPins():
+    wrap_target()
+    set(0,0)
+    set(0,1)
+    #set(1,0)    
+    #set(1,1)    
+    wrap()
+
+
+sm1 = StateMachine(1, myPins, freq=2_000_000, set_base=Pin(2))
+sm1.active(1)
+sm2 = StateMachine(0, myPins, freq=2_000_000, set_base=Pin(3))
+sm2.active(1)
+
+while True:
+    time.sleep_ms(100)
--- pioRework.py
+++ pioRework.py
@@ -25,26 +25,27 @@ SM_FIFO_RXEMPTY = const(0x00000100)
 SM_FIFO_TXFULL  = const(0x00010000)
 SM_FIFO_TXEMPTY = const(0x01000000)
 
-@rp2.asm_pio(set_init=(rp2.PIO.IN_LOW,rp2.PIO.OUT_LOW, rp2.PIO.IN_LOW), autopush=True,push_thresh=8, in_shiftdir=rp2.PIO.SHIFT_LEFT)
-def IEC_CBM_Bus():
+@rp2.asm_pio(set_init=(rp2.PIO.IN_LOW,rp2.PIO.IN_LOW, rp2.PIO.IN_LOW), autopush=True,push_thresh=8, in_shiftdir=rp2.PIO.SHIFT_LEFT)
+def IEC_CBM_Bus2():
     ## reminder of instruction args in micropython:
     #wait(polarity, src, index)
     #in_(src, bit_count)
     #set(dest, data)
     ## setting pindirs = decimal 2 = binary 10 = base+1 = data = output, clock = 0 = input
-    ## setting pindirs = decimal 0 = binary 0 = base+1 = data = input, clock = 0 = input   
+    ## setting pindirs = decimal 0 = binary 0 = base+1 = data = input, clock = 0 = input
     
     ### next line commented out for now just deal with clock and data
-    ##wait(0, pins, 2) ## wait for ATN to go low (true) but only on bus command
+#    wait(0, pins, 2) ## wait for ATN to go low (true) but only on bus command
     set(pindirs, 0b10)   # clock input - data output
     
     wrap_target()  
-    set(1,0)       [31]   # when ready to receive the listener sets data pin to TRUE "I'm here"
+    set(1,0)        [31] # when ready to receive the listener sets data pin to TRUE "I'm here"
     #########
     ## initial state before the instigation of any data transfer
     #########
+    
     wait(1, pin, 0)   # wait for clock pin FALSE=1 (on pin 0) set by talker
-    set(1,1)       [31]   # when ready to receive the listener sets data pin to FALSE = 1
+    set(1,1)          [31]# when ready to receive the listener sets data pin to FALSE = 1
         
     set(pindirs, 0)  # set both clock and data to input
       
@@ -57,32 +58,40 @@ def IEC_CBM_Bus():
     
     set(pindirs, 0b10)   # clock input - data output
     set(1,0)   [31]            # hold data line false
+
     wrap()
  
-
+#@rp2.asm_pio(set_init=(rp2.PIO.OUT_LOW,rp2.PIO.OUT_HIGH), autopush=True,push_thresh=8, in_shiftdir=rp2.PIO.SHIFT_LEFT)
+#@rp2.asm_pio(set_init=(rp2.PIO.OUT_LOW,rp2.PIO.OUT_LOW), out_init=(rp2.PIO.OUT_LOW,rp2.PIO.OUT_LOW), autopush=True,push_thresh=8, in_shiftdir=rp2.PIO.SHIFT_LEFT)
+@rp2.asm_pio(set_init=(rp2.PIO.OUT_LOW,rp2.PIO.OUT_LOW))  
+def IEC_CBM_Bus():
+    ## reminder of instruction args in micropython:
+    #wait(polarity, src, index)
+    #in_(src, bit_count)
+    #set(dest, data)
+    #out(dest, bit_count)
+    #mov(dest, src)
+    
+    
+    set(pindirs,0b11)
+    wrap_target()
+    
+    ## does not work for any output other than the base pin
+    set(1,0)        [10] # when ready to receive the listener sets data pin to TRUE "I'm here"
+    set(1,1)        [10] # when ready to receive the listener sets data pin to TRUE "I'm here"
+    set(0,0)        [3] # when ready to receive the listener sets data pin to TRUE "I'm here"
+    set(0,1)        [3] # when ready to receive the listener sets data pin to TRUE "I'm here"    
+    wrap()
+      
     
 class IEC1541Pio():
-    def __init__(self, clockGPIO, DataGPIO, ATN_GPIO, instance):
-        ## set the pins up although data pin overwritten by SM handshaking 
-        myPin = Pin(clockGPIO,Pin.IN)
-        myPin.value(1)
-        utime.sleep_ms(1)
-        #myPin.init(Pin.IN,Pin.PULL_DOWN)
-
-        myPin = Pin(DataGPIO,Pin.IN)
-        myPin.value(1)
-        utime.sleep_ms(1)
-        #myPin.init(Pin.OUT,Pin.PULL_DOWN)
-        
-        myPin = Pin(ATN_GPIO,Pin.IN)
-        myPin.value(1)
-        utime.sleep_ms(1)
-        #myPin.init(Pin.IN,Pin.PULL_DOWN)
-                
-        utime.sleep_ms(100)
-        
+    #def __init__(self, clockGPIO, DataGPIO, ATN_GPIO, instance):
+    def __init__(self, instance, clockGPIO, DataGPIO, ATN_GPIO):
         # running on the 2nd pio block which is state machine 4-7, instance count allows multiple
-        self.sm = rp2.StateMachine(instance, IEC_CBM_Bus, freq=2000000,in_base=Pin(clockGPIO), set_base=Pin(clockGPIO))        
+        # fails 1 self.sm = rp2.StateMachine(instance, IEC_CBM_Bus, freq=2000000,set_base=Pin(clockGPIO))
+        # fails 2 self.sm = rp2.StateMachine(instance, IEC_CBM_Bus, freq=2000000,set_base=Pin(DataGPIO))
+        # OK self.sm = rp2.StateMachine(instance, IEC_CBM_Bus, freq=2000000,set_base=Pin(DataGPIO))
+        self.sm = rp2.StateMachine(instance, IEC_CBM_Bus, freq=2000000,set_base=Pin(clockGPIO))        
         
         self.sm.active(1)
         self.tx_Data = 0x00
@@ -95,10 +104,12 @@ class IEC1541Pio():
         self.count = self.count + 1
         return self.rx_Data
             
-
-iec_bus = IEC1541Pio(2,3,4,0)
+StateMachineId = 1
+iec_bus = IEC1541Pio(StateMachineId,2,3,4)
+#myObj = PIOSPI(0)
 smx0 = PIO0_BASE + 0xd8   #d4=0 ec=1   d8,f0 = instr
 
 while True:    
-    print(hex(iec_bus.getData()))    
+    #print(hex(iec_bus.getData()))
+    x = 1
     
