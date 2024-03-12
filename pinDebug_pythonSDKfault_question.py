adf04cfcac3fc7300fbd9617f25d288b6445770e
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2024-03-12 19:06:46 +0000

    got correct initial sequence of bytes for first time

--- pinDebug_pythonSDKfault_question.py
+++ pinDebug_pythonSDKfault_question.py
@@ -10,16 +10,30 @@ import time
 #out_init configures the pins used for out() instructions.
 #set_init configures the pins used for set() instructions. There can be at most 5.
 
-#current state of code is it outputs this when you issue LOAD"A",8 but only on first time
-#MPY: soft reboot
-#1 0b10100 0x28     << correct
-#2 0b10000111 0xe1  << wrong should be 0xf0
-#3 0b11111111 0xff  << should be filename bytes "A" in petsci
-#4 0b100010 0x44    << should be 0x3f unlisten
-#5 0b1010000 0xa    << should be 48 tells device 8 to talk
+#current state of code is it outputs this when you issue LOAD"A",8
 
+## correct sequence is:
+# /28	Device	Listen, device number 8
+# /F0	Device	Open channel 0
+# Device	Send filename bytes
+# /3F	Devices	Unlisten all devices
+# /48	Device	Talk, Device number 8
+# /60	Device	Reopen channel 0
+
+# MPY: soft reboot
+# 1 0xaa 0b10101010 0x55
+# 2 0x14 0b10100 0x28
+# 3 0x1aa 0b110101010 0x55
+# 4 0xf 0b1111 0xf0
+# 5 0x1aa 0b110101010 0x55
+# 6 0xfc 0b11111100 0x3f
+# 7 0x1aa 0b110101010 0x55
+# 8 0x12 0b10010 0x48
+# 9 0x1aa 0b110101010 0x55
+# 10 0x6 0b110 0x60
 
 @asm_pio(set_init=(PIO.IN_LOW,PIO.IN_LOW,PIO.OUT_LOW),autopush=True, push_thresh=8,in_shiftdir=PIO.SHIFT_LEFT)
+#@asm_pio(set_init=(PIO.IN_LOW,PIO.IN_LOW,PIO.OUT_LOW),push_thresh=8,in_shiftdir=PIO.SHIFT_LEFT)
 def myPins():
     #wait(0, gpio, 4)   # wait for ATN true    	
     wrap_target()
@@ -32,6 +46,13 @@ def myPins():
     
     wait(0, gpio, 2)   # wait for clock true
     #step 1
+    ##DEBUG fil fifo with a progress counter - this will be read by main loop (we have autopush 8 set 
+    set(y,0b1010)
+    in_(y, 4)
+    in_(y, 4) 
+    #push()
+    ##
+      
     wait(1, gpio, 2)  # wait for clock false - signals that talker ready to send
     set(pins,0b000)    [26]# set data to true
     set(pins,0b000)    [26]# set data to true
@@ -39,21 +60,20 @@ def myPins():
     set(pins,0b000)    [13]# set data to true
     #set(pins,0b00)    [26]# set data to true    
     #step 2
-    set(pins,0b100)   # set data to false
-    set(pindirs,0b000)  # set both pins to inputs    
-    set(x,8)
+    set(pins,0b100)   [26] # set data to false
+    set(pindirs,0b000)  # set both pins to inputs
     wait(0, gpio, 2)
+    
+    set(x,8)    
     label("bitReadStart")
     ## read the bits   =least LSB first and true 0volts= bit set to 1, false = 5volts = bit set to 0
     wait(1, gpio, 2)   # wait for clock to go false
     in_(pins, 1)       # input one bit from GPIO 3 
     wait(0, gpio, 2)   # wait for clock to go true
-    jmp(x_dec,"bitReadStart")
     
-    ##DEBUG fil fifo with a progress counter - this will be read by main loop (we have autopush 8 set 
-    #set(x,4)
-    #in_(x, 8)
-    ##    
+    jmp(x_dec,"bitReadStart")
+    #push()
+    
     # step 4
     wait(0, gpio, 2)    # wait for clock true
     wrap()
@@ -75,4 +95,4 @@ while True:
         msb_to_lsb = 0
         for i in range(8):
             msb_to_lsb |= ((inverted_bits >> i) & 1) << (7 - i)
-        print(count, bin(rawValue), hex(msb_to_lsb))
+        print(count, hex(rawValue), bin(rawValue), hex(msb_to_lsb))
